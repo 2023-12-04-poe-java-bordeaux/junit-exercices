3844b9758a0a3799884a29f2266c39572cee897b
jcdominguez <jcd.pro@gmail.com>
2024-01-10 11:06:31 +0100

    additionner()

---
+++ src/test/java/com/demo/CalculatriceTest.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatriceTest {
+
+
+    @Test
+    void testAdditionner() {
+        assertEquals(5, Calculatrice.additionner(2,3));
+    }
+
+    @Test
+    void testAddtionnerDeuxAvecZero(){
+        assertEquals(2, Calculatrice.additionner(2,0));
+    }
+
+    @Test
+    void testAdditionnerAvecNombreNegatif(){
+        assertEquals(-6, Calculatrice.additionner(-2, -4));
+    }
+}
